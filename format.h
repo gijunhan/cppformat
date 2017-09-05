29fd3e734d67f0fdb6de78740701105943373af0
Gijun Han <gijun.han@intra.gree-office.net>
2017-09-05 11:47:21 +0900

    win32

--- format.h
+++ format.h
@@ -1,2 +1,6 @@
 #include "cppformat/format.h"
+#if _MSC_VER
+#pragma message ("Warning: Includeing format.h from the top-level directory is deprecated")
+#else
 #warning Including format.h from the top-level directory is deprecated.
+#endif
